22f7ce26517fbde8156c1be1139dc712996ea60b
jazzywuboo <normanj2@vcu.edu>
2018-04-19 23:06:12 -0400

    trying out function to auto calibrate scale

--- scale/scale.py
+++ scale/scale.py
@@ -77,7 +77,7 @@ def get_weight():
 if __name__ == "__main__":
     hx = HX711(5, 6)
     hx.set_offset(8334388.297)
-    hx.set_scale(-13.4)
+    hx.set_scale(-14.5)
 
     while True:
         try:
