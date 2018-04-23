f32e0aaeae4e1ed495d65b331cc285c829aa0b39
jazzywuboo <normanj2@vcu.edu>
2018-04-23 12:48:54 -0400

    editing paths for testing

--- env_vars.py
+++ env_vars.py
@@ -1,11 +1,5 @@
-import os
-
 port_num = 3003
 
-# if running server on pi
-# host = '192.168.43.87'
-# base_path = '/home/pi/Desktop/VMI_simulator/'
-
-# if running server on localhost
-host = 'localhost'
-base_path = os.path.dirname(__file__) + '/'
+host = '192.168.0.13'   # waleed's
+# host = '192.168.43.87'  # hotspot
+base_path = '/home/pi/Desktop/VMI_simulator/'
