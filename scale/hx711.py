18ff86136c7e7248a5e91ce61b69096b0296b4e0
jazzywuboo <normanj2@vcu.edu>
2018-04-24 00:45:09 -0400

    testing calibration

--- scale/hx711.py
+++ scale/hx711.py
@@ -106,7 +106,7 @@ class HX711:
         self.power_down()
         time.sleep(0.001)
         self.power_up()
-        time.sleep(3)
+        time.sleep(2)
 
     def set_offset(self, offset):
         """
